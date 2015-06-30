ec94e06cac75eec574a0998f9a952061889022c2
maclover7 <me@jonathanmoss.me>
2015-06-29 21:31:47 -0400

    Updated NJ Transit jobs

--- jobs/outbound.rb
+++ jobs/outbound.rb
@@ -1,10 +1,9 @@
-if Time.now.strftime("%H").between?("17", "19") 
-  SCHEDULER.every '30s', :first_in => 0 do |job|
-    status = TrainStatus.new(ENV["NJT_OUTBOUND_STATION"], ENV["NJT_OUTBOUND_NUMBER"]).get_status!(ENV["NJT_OUTBOUND_STATION"], ENV["NJT_OUTBOUND_NUMBER"])
-    send_event('outbound', {status: "#{status}" })
-  end
-
-else
-  status = "n/a"
-  send_event('outbound', {status: "#{status}" })
-end
+ SCHEDULER.every '30s', :first_in => 0 do |job|
+   if Time.now.strftime("%H").between?("17", "19")
+     status = TrainStatus.new(ENV["NJT_OUTBOUND_STATION"], ENV["NJT_OUTBOUND_NUMBER"]).get_status!(ENV["NJT_OUTBOUND_STATION"], ENV["NJT_OUTBOUND_NUMBER"])
+     send_event('outbound', {status: "#{status}" })
+   else
+     status = "n/a"
+     send_event('outbound', {status: "#{status}" })
+   end
+ end
